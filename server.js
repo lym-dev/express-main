6671a19063d2fb59a85e9ed7b0627b59d0ca070f
dev <goldenchange01@gmail.com>
2025-02-10 20:21:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post('/', async (req, res) => {
   try {
     // Forward the request to the service worker
     const response = await axios.post(
-      'https://lym-dev.github.io/express-main/worker.js/', // Replace with your service worker URL
+      'https://express-main.vercel.app/worker.js/', // Replace with your service worker URL
       req.body, // Forward the request body as is
       {
         headers: {
